8f534b36b06edf3450be0533c4892f30de9ad3b9
Eirik Tvedt Jensen <eirik.jet@gmail.com>
2014-03-11 12:24:16 +0100

    slettet testfil og la til Gruppe.java i mappen modell

---
+++ src/modell/Gruppe.java
@@ -0,0 +1,29 @@
+package modell;
+
+public class Gruppe {
+
+	private Person[] personListe;
+	private int gruppeID;
+	private String gruppeNavn;
+	
+	public Gruppe(Person[] personListe, int gruppeID, String gruppeNavn){
+		
+		this.personListe=personListe;
+		this.gruppeID=gruppeID;
+		this.gruppeNavn=gruppeNavn;
+	}
+	
+	public int getGruppeID() {
+		return gruppeID;
+	}
+	
+	public String getGruppeNavn() {
+		return gruppeNavn;
+	}
+
+	public Person[] getPersonListe() {
+		return personListe;
+	}
+	
+	
+}

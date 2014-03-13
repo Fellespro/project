a9d70110120dcaf526c4b7e2222f046c5f21e484
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2014-03-13 11:21:16 +0100

    La inn toString() i Dato, for bruk mot databasen

--- src/modell/Dato.java
+++ src/modell/Dato.java
@@ -1,5 +1,21 @@
 package modell;
 
 public class Dato {
+	private int year;
+	private int month;
+	private int day;
+	
+	public Dato(){
+		
+	}
+	
+	public String toString(){
+		//Det er mulig at dersom f.eks. day<10, så må toString() skrive ut
+		//day som 0x.
+		//Altså: er 2014-2-7 gyldig?
+		//Eller må det skrives som: 2014-02-07 ..?
+		String out = ""+year+"-"+month+"-"+day+"";
+		return out;
+	}
 
 }

f4dfa6df0a5074717a3b0bf33bd93b7510261436
Ole Steinar Lillestøl Skrede <oleskrede@gmail.com>
2014-03-13 11:29:20 +0100

    La til toString() i Tid

--- src/modell/Tid.java
+++ src/modell/Tid.java
@@ -1,5 +1,17 @@
 package modell;
 
 public class Tid {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public Tid(){
+		
+	}
+	
+	public String toString(){
+		String out = ""+hour+":"+minute+":"+second;
+		return out;
+	}
 
 }
